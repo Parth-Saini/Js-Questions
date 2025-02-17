412c02d5a727c3e844b889c7a1e24899e01328a9
Parth-Saini <sainiparth018@gmail.com>
2025-02-17 23:55:01 +0530

    (17-02-2025) Test

---
+++ Day 3 (17-02-2025)/Loops.js
@@ -0,0 +1,22 @@
+const str = "ankush";
+
+const myFunction = (name) => {
+    if (name%2 !== 0) {
+    const firstChar = name.charAt(0).toUpperCase();
+    const lastChar = name.charAt(name.length - 1).toUpperCase();
+    const secondChar = name.slice(1,Math.floor(name.length/2)).toLowerCase();
+    const middle = name.charAt(Math.ceil(name.length/2)).toUpperCase();
+    const forth = name.slice(Math.ceil(name.length/2),name.length-1).toLowerCase();
+    const output = firstChar + secondChar + middle + forth + lastChar;
+    } 
+    else if (name%2 == 0) {
+        const firstChar = name.charAt(0).toUpperCase();
+        const lastChar = name.charAt(name.length - 1).toUpperCase();
+        const secondChar = name.slice(1,Math.floor(name.length/2)).toLowerCase();
+        const middle = name.charAt(Math.ceil(name.length/2)).toUpperCase();
+        const forth = name.slice(Math.ceil(name.length/2),name.length).toLowerCase();
+        const output = firstChar + secondChar + middle + forth + lastChar;
+    }
+    return output;
+}
+console.log(myFunction(str));
