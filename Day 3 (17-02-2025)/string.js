c3e167f08b10550e99a0bf62d5d99e878ccf938e
Parth-Saini <sainiparth018@gmail.com>
2025-02-18 12:58:15 +0530

    udated

--- Day 3 (17-02-2025)/string.js
+++ Day 3 (17-02-2025)/string.js
@@ -12,9 +12,9 @@ const myFunction = (name) => {
     else if (name%2 == 0) {
         const firstChar = name.charAt(0).toUpperCase();
         const lastChar = name.charAt(name.length - 1).toUpperCase();
-        const secondChar = name.slice(1,Math.floor(name.length/2)).toLowerCase();
-        const middle = name.charAt(Math.ceil(name.length/2)).toUpperCase();
-        const forth = name.slice(Math.ceil(name.length/2),name.length).toLowerCase();
+        const secondChar = name.slice(1,(name.length/2)).toLowerCase();
+        const middle = name.charAt((name.length/2)-1).toUpperCase();
+        const forth = name.charAt(name.length/2).toUpperCase();
         const output = firstChar + secondChar + middle + forth + lastChar;
     }
     return output;
